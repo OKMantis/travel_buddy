61696a24d0a27881ba97c1edba0a731a95a38aa5
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-11-26 19:21:57 +0100

    llm now gets the activities

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -7,6 +7,12 @@ class ChatsController < ApplicationController
   end
   
   def create
+    
+    @chat = Chat.new(chat_params)
+    @chat.user = current_user
+    @city = @chat.city
+    @category = @chat.category
+    @season = @chat.season
     @activities = Activity
     .where(city: @city, category: @category, season: @season)
     .map do |activity|
@@ -19,17 +25,16 @@ class ChatsController < ApplicationController
       }
     end
     
-    @chat = Chat.new(chat_params)
-    @chat.user = current_user
-    @city = @chat.city
-    @category = @chat.category
-    @season = @chat.season
-    @system_prompt = "You are a Activities Assistant.\n\nI am a traveler , looking for activities to do in a chosen city.\n\nProvide a list of activities in #{@city} that fit the category #{@category} and are appropriate for the #{@season} season. Include only the activities from this list: #{@activities}"
-    @user_prompt = "Provide a list of activities"
+    @system_instructions = "You are a Activities Assistant.\n\nI am a traveler , looking for activities to do in a chosen city."
+    
+    @system_prompt = "Provide a list of activities in #{@city} that fit the category #{@category} and are appropriate for the #{@season} season. Include only the activities from this list: #{@activities}"
+
+    @user_message = "Provide a list of activities in #{@city} that fit the category #{@category} and are appropriate for the #{@season} season."
+    
 
     ruby_llm_chat = RubyLLM.chat
-    response = ruby_llm_chat.with_instructions(@system_prompt).ask(@user_prompt)
-    Message.create(role: "user", content: @user_prompt, chat: @chat)
+    response = ruby_llm_chat.with_instructions(@system_instructions).ask(@system_prompt)
+    Message.create(role: "user", content: @user_message, chat: @chat)
     Message.create(role: "assistant", content: response.content, chat: @chat)
 
     if @chat.save
