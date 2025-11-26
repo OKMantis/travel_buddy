dca6c8d5f4c94883ce4a78436f9a22af45ff2d94
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-11-26 12:43:38 +0100

    finished basic response llm

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,13 +1,30 @@
 class ChatsController < ApplicationController
+
+  SYSTEM_PROMPT = "You are a Activities Assistant.\n\nI am a traveler , looking for activities to do in a chosen city.\n\nAnswer concisely in Markdown."
   
   def show
-    @chat    = current_user.chats.find(params[:id])
+    @chat = current_user.chats.find(params[:id])
     @message = Message.new
   end
 
   def create
     @chat = Chat.new(chat_params)
     @chat.user = current_user
+    @city = @chat.city
+    @category = @chat.category
+    @season = @chat.season
+    @user_prompt = "Provide a list of activities in #{@city} that fit the category #{@category} " \
+    "and are appropriate for the #{@season} season. Include only activities that " \
+    "are realistically available in that city during that season. " \
+    "Return the result as a list, and for each activity include: " \
+    "- the activity name, " \
+    "- a brief description, " \
+    "- a direct link to its official or most relevant URL."
+
+    ruby_llm_chat = RubyLLM.chat
+    response = ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@user_prompt)
+    Message.create(role: "user", content: @user_prompt, chat: @chat)
+    Message.create(role: "assistant", content: response.content, chat: @chat)
 
     if @chat.save
       redirect_to chat_path(@chat)
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,32 +3,14 @@ class MessagesController < ApplicationController
 
   def create
     @chat = current_user.chats.find(params[:chat_id])
-    @city = @chat.city
-    @category = @chat.category
-    @season = @chat.season
-    # @activity_url = Activity.where()
 
     @message = Message.new(message_params)
     @message.chat = @chat
     @message.role = "user"
-    ruby_llm_chat = RubyLLM.chat
-    response = ruby_llm_chat
-    .with_instructions(instructions).ask(
-      "Provide a list of activities in #{@city} that fit the category #{@category} " \
-      "and are appropriate for the #{@season} season. Include only activities that " \
-      "are realistically available in that city during that season. " \
-      "Return the result as a list, and for each activity include: " \
-      "- the activity name, " \
-      "- a brief description, " \
-      "- a direct link to its official or most relevant URL."
-    )
-    Message.create(role: "assistant", content: response.content, chat: @chat)
-    redirect_to chat_messages_path(@chat)
-
+    
     if @message.save
       ruby_llm_chat = RubyLLM.chat
-      response = ruby_llm_chat.with_instructions(instructions).ask(@message.content)
-      # response = ruby_llm_chat.ask(@message.content)
+      response = ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
       Message.create(role: "assistant", content: response.content, chat: @chat)
 
       redirect_to chat_messages_path(@chat)
