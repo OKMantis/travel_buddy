5fcbb53058b65777072770016a559384914a4cf6
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-11-26 17:50:02 +0100

    fixing llm

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,28 +1,33 @@
 class ChatsController < ApplicationController
-
-  SYSTEM_PROMPT = "You are a Activities Assistant.\n\nI am a traveler , looking for activities to do in a chosen city.\n\nAnswer concisely in Markdown."
+  
   
   def show
     @chat = current_user.chats.find(params[:id])
     @message = Message.new
   end
-
+  
   def create
+    @activities = Activity
+    .where(city: @city, category: @category, season: @season)
+    .map do |activity|
+      {
+        id: activity.id,
+        content: activity.content,
+        category: activity.category,
+        city: activity.city,
+        season: activity.season
+      }
+    end
     @chat = Chat.new(chat_params)
     @chat.user = current_user
     @city = @chat.city
     @category = @chat.category
     @season = @chat.season
-    @user_prompt = "Provide a list of activities in #{@city} that fit the category #{@category} " \
-    "and are appropriate for the #{@season} season. Include only activities that " \
-    "are realistically available in that city during that season. " \
-    "Return the result as a list, and for each activity include: " \
-    "- the activity name, " \
-    "- a brief description, " \
-    "- a direct link to its official or most relevant URL."
+    @system_prompt = "You are a Activities Assistant.\n\nI am a traveler , looking for activities to do in a chosen city.\n\nProvide a list of activities in #{@city} that fit the category #{@category} and are appropriate for the #{@season} season. Include only the activities from this list: #{@activities}"
+    @user_prompt = "Provide a list of activities"
 
     ruby_llm_chat = RubyLLM.chat
-    response = ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@user_prompt)
+    response = ruby_llm_chat.with_instructions(@system_prompt).ask(@user_prompt)
     Message.create(role: "user", content: @user_prompt, chat: @chat)
     Message.create(role: "assistant", content: response.content, chat: @chat)
 
