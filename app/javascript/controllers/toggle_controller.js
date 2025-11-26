4b8feb85a716650e391dbb7307ccb989558a22e2
Roukia Sabry <roukia.raia@gmail.com>
2025-11-26 17:38:01 +0100

    i forgot to put the m in the last commit, but now this is the save with the changes of the sidebar and home page

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="toggle"
+export default class extends Controller {
+  static targets = ["hideable"]
+
+  connect() {
+    console.log("Hello from toggle_controller.js")
+  }
+
+  call(e) {
+    e.preventDefault()
+    this.hideableTarget.classList.toggle("d-none")  // bootstrap hide
+  }
+}
