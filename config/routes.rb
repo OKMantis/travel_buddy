3ba30fdaf07fc626f7b89679b5e9b864e36d2f13
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-11-27 14:30:41 +0100

    commit merge

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,6 @@ Rails.application.routes.draw do
   resources :chats, only: [:create, :show, :destroy] do
     resources :messages, only: [:create]
   end
+
+  resources :travel_books, only: :create
 end
